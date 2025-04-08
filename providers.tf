7d3b800371b2955a3641f0febaea05b73fa3da6d
sid14581 <siddhardh.kbv@gmail.com>
2025-04-08 22:01:24 +0530

    changes in provider.tf file

--- providers.tf
+++ providers.tf
@@ -2,15 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>3.0"
+      version = "4.4.0"
     }
-  }
-
-  backend "s3" {
-    bucket = "ec2-with-sgp-example-tf-state-one"
-    region = "us-east-2"
-    key    = "env/dev/cloudgeeks-dev.tfstate"
-    # dynamodb_table = "cloudgeeks-dev-terraform-backend-state-lock"
   }
 }
 
@@ -18,4 +11,11 @@ provider "aws" {
   region = "us-east-2"
 }
 
-
+terraform {
+  backend "s3" {
+    bucket = "ec2-with-sgp-example-tf-state-one"
+    region = "us-east-2"
+    key    = "env/dev/cloudgeeks.tfstate"
+    # dynamodb_table = "cloudgeeks-dev-terraform-backend-state-lock"
+  }
+}
