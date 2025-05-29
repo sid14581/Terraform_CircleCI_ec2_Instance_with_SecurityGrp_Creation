b54ebccc1f572628012f3edeaa396a8b44f0c8ef
sid14581 <siddhardh.kbv@gmail.com>
2025-05-29 19:13:42 +0530

    creating ec2 with sgp

--- vpc.tf
+++ vpc.tf
@@ -1,27 +1,27 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.12.0"
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "3.12.0"
 
-  name = "sample-vpc"
+#   name = "sample-vpc"
 
-  cidr            = "10.60.0.0/16"
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  private_subnets = ["10.60.0.0/23", "10.60.2.0/23", "10.60.4.0/23"]
-  public_subnets  = ["10.60.100.0/24", "10.60.101.0/24", "10.60.102.0/24"]
+#   cidr            = "10.60.0.0/16"
+#   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+#   private_subnets = ["10.60.0.0/23", "10.60.2.0/23", "10.60.4.0/23"]
+#   public_subnets  = ["10.60.100.0/24", "10.60.101.0/24", "10.60.102.0/24"]
 
 
-  map_public_ip_on_launch = true
-  enable_nat_gateway      = true
-  single_nat_gateway      = true
-  one_nat_gateway_per_az  = false
+#   map_public_ip_on_launch = true
+#   enable_nat_gateway      = true
+#   single_nat_gateway      = true
+#   one_nat_gateway_per_az  = false
 
-  create_database_subnet_group           = true
-  create_database_subnet_route_table     = true
-  create_database_internet_gateway_route = false
-  create_database_nat_gateway_route      = true
+#   create_database_subnet_group           = true
+#   create_database_subnet_route_table     = true
+#   create_database_internet_gateway_route = false
+#   create_database_nat_gateway_route      = true
 
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
+# }
 
 
