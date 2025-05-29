56cd9d05d4b60daa5283bf5fcc979d798492ad32
sid14581 <siddhardh.kbv@gmail.com>
2025-05-29 18:54:36 +0530

    creating ec2 with sgp

--- securitygrp.tf
+++ securitygrp.tf
@@ -1,37 +1,37 @@
-# module "securitygroup" {
-#   source  = "terraform-aws-modules/security-group/aws"
-#   version = "3.0.1"
-  
-#   name = "ec2-web-sgp"
-#   vpc_id = module.vpc.vpc_id
+module "securitygroup" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "3.0.1"
 
-#   computed_ingress_with_cidr_blocks = [ 
-#     {
-#       from_port   = 80
-#       to_port     = 80
-#       protocol    = 6
-#       description = "Web HTTP"
-#       cidr_blocks = "0.0.0.0/0"
-#     },
-#     {
-#       from_port   = 443
-#       to_port     = 443
-#       protocol    = 6
-#       description = "Web HTTPS"
-#       cidr_blocks = "0.0.0.0/0"
-#     },
-#   ]
+  name   = "ec2-web-sgp"
+  vpc_id = module.vpc.vpc_id
 
-#   number_of_computed_ingress_with_cidr_blocks = 2
+  computed_ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = 6
+      description = "Web HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = 6
+      description = "Web HTTPS"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
 
-#   computed_egress_with_cidr_blocks = [
-#     {
-#         from_port = 0
-#         to_port = 0
-#         protocol = -1
-#         description = "All Out Bound Allowed"
-#         cidr_blocks = "0.0.0.0/0"
-#     }
-#   ]
-#   number_of_computed_egress_with_cidr_blocks = 1    
-# }
+  number_of_computed_ingress_with_cidr_blocks = 2
+
+  computed_egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = -1
+      description = "All Out Bound Allowed"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  number_of_computed_egress_with_cidr_blocks = 1
+}
